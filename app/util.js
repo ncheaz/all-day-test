839c823b22b22382c414a25185dd316b066bfb7d
RJ Miller <rjmiller10@gmail.com>
2016-11-15 13:41:31 -0500

    Reorganize util.js

--- app/util.js
+++ app/util.js
@@ -6,55 +6,73 @@ const rp = require('request-promise');
 const _ = require('lodash');
 
 
-const baseUrl = "http://www.anapioficeandfire.com/api"
-const characterPath = "/characters"
+// TODO: move to own file
+/**
+ * Get the options with a specific page number
+ */
+const getOptions = (() => {
+  const baseUrl = "http://www.anapioficeandfire.com/api"
+  const characterPath = "/characters"
 
-const options = {
-  uri: baseUrl + characterPath,
-  qs: {
-    pageSize: 50,
-  },
-  json: true
-}
-
-// TODO: better name
-const getOptions = (page) => {
-  let optClone = _.cloneDeep(options);
-  optClone.qs.page = page;
-  return optClone
-}
-
-var pageCounter = 1;
-var cacheArray = []
-
-// TODO: better name
-const callback = (res) => {
-  if (_.isEqual(res, [])) {
-    return Promise.reject(false);
+  const options = {
+    uri: baseUrl + characterPath,
+    qs: {
+      pageSize: 50,
+    },
+    json: true
   }
-  pageCounter++;
-  // TODO: use lodash merge
-  cacheArray = cacheArray.concat(res);
+
+  // TODO: better name
+  const getOptions = (page) => {
+    let optClone = _.cloneDeep(options);
+    optClone.qs.page = page;
+    return optClone
+  }
+  
+  return getOptions
+})();
+
+let apiCacheData = [];
+let quotesCacheData = [];
+
+/**
+ * Get data from API and store it in cache.json
+ */
+const cacheDataFromApi = () => {
+  let pageCounter = 1;
+
+  // TODO: better name
+  const callback = (res) => {
+    if (_.isEqual(res, [])) {
+      return Promise.reject(false);
+    }
+    console.dir(res);
+    pageCounter++;
+    // TODO: use lodash merge
+    apiCacheData = apiCacheData.concat(res);
+    return rp(getOptions(pageCounter))
+      .then(callback);
+  }
+
   return rp(getOptions(pageCounter))
-    .then(callback);
+    .then(callback)
+    .catch((err) => {
+      if (!err) { // reached last page of API
+        console.log("not an error"); // TODO: better log message
+        return writeFile('./cache.json', apiCacheData, {spaces: 2})
+          .then(() => {
+            console.log('file write successful'); // TODO: better log message
+          })
+          .catch((err) => {
+            console.error(err);
+          })
+      } else {
+        console.error(err);
+      }
+    })
 }
 
-rp(getOptions(pageCounter))
-  .then(callback)
-  .catch((err) => {
-    if (!err) {
-      console.log("not an error"); // TODO: better log message
-      return writeFile('./cache.json', cacheArray, {spaces: 2})
-        .then(() => {
-          console.log('file write successful'); // TODO: better log message
-        })
-        .catch((err) => {
-          console.error(err);
-        })
-    } else {
-      console.error(err);
-    }
-  })
+cacheDataFromApi()
 
 // TODO: write function that takes character name (as in quotes.json) and grabs
 // character data from API
